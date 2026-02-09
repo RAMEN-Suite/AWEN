9267c0558fbdb78046f4c39c42f50f21c82aa85f
VNxyz1 <vincenzo_n@rocketmail.com>
2026-02-09 15:54:16 +0100

    feat validate schema against gcore.json

--- server/src/schema/schema-loader.service.ts
+++ server/src/schema/schema-loader.service.ts
@@ -3,6 +3,7 @@ import { GModel } from './interfaces/g-model.interface';
 import * as fs from 'node:fs';
 import { ModelRegistry } from './model.registry';
 import path from 'node:path';
+import { compileSchema, JsonSchema, SchemaNode } from 'json-schema-library';
 
 @Injectable()
 export class SchemaLoaderService {
@@ -17,15 +18,22 @@ export class SchemaLoaderService {
   }
 
   private loadSchemas() {
+    const gCoreSchemaJSON: JsonSchema = JSON.parse(
+      fs.readFileSync(this.gCorePath(), 'utf8'),
+    ) as JsonSchema;
+    const gCoreSchema: SchemaNode = compileSchema(gCoreSchemaJSON);
     this.logger.log('Loading schemas...');
-    const ramen: GModel = JSON.parse(
-      fs.readFileSync(this.ramenPath(), 'utf8'),
-    ) as GModel;
+
+    const ramen: GModel = this.loadAndValidate<GModel>(
+      this.ramenPath(),
+      gCoreSchema,
+    );
     this.logger.log(`Loaded ramen schema "${ramen.version}" successfully`);
 
-    const profile: GModel = JSON.parse(
-      fs.readFileSync(this.profilePath(), 'utf8'),
-    ) as GModel;
+    const profile: GModel = this.loadAndValidate<GModel>(
+      this.profilePath(),
+      gCoreSchema,
+    );
     this.logger.log(
       `Loaded project schema "${profile.name} ${profile.version}" successfully`,
     );
@@ -40,11 +48,30 @@ export class SchemaLoaderService {
     return this.registry;
   }
 
+  private gCorePath() {
+    return path.join(process.cwd(), 'dist', 'config', 'gcore.json');
+  }
+
   private ramenPath() {
     return path.join(process.cwd(), 'dist', 'config', 'ramen.gcore.json');
   }
 
   private profilePath() {
     return path.join(process.cwd(), 'dist', 'config', 'project.gcore.json');
+  }
+
+  private loadAndValidate<T>(path: string, schema: SchemaNode): T {
+    const json = JSON.parse(fs.readFileSync(path, 'utf8')) as unknown;
+
+    const result = schema.validate(json);
+
+    if (!result.valid) {
+      for (const err of result.errors) {
+        this.logger.error(`Schema error: ${err.message}`, err.data);
+      }
+      throw new Error('Invalid schema');
+    }
+
+    return json as T;
   }
 }
