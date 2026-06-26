35872c2901426e0695d43cf5e2eacff6486792f5
VNxyz1 <vincenzo_n@rocketmail.com>
2026-06-26 12:42:55 +0200

    feat: dynamic textsize and better spacing

--- client/src/app/statements/property-list/property-list.ts
+++ client/src/app/statements/property-list/property-list.ts
@@ -1,7 +1,9 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { Chip } from 'primeng/chip';
 import { visibleProperties } from '../../utils/utils';
 import { NodePropertyDto } from '../../../interfaces';
+
+type TextSize = 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
 @Component({
   selector: 'app-property-list',
@@ -10,6 +12,15 @@ import { NodePropertyDto } from '../../../interfaces';
 })
 export class PropertyList {
   properties = input.required<NodePropertyDto[]>();
+  textSize = input<TextSize>(3);
+
+  protected lableTextSizeClass = computed(() => {
+    return this.getTextSizeClass(this.textSize() - 2);
+  });
+
+  protected valueTextSizeClass = computed(() => {
+    return this.getTextSizeClass(this.textSize());
+  });
 
   protected readonly visibleProperties = visibleProperties;
 
@@ -19,5 +30,30 @@ export class PropertyList {
 
   protected displayValue(value: unknown): string {
     return String(value ?? '');
+  }
+
+  private getTextSizeClass(textSize: number) {
+    switch (textSize) {
+      case 1:
+        return 'text-xs';
+      case 2:
+        return 'text-sm';
+      case 3:
+        return 'text-base';
+      case 4:
+        return 'text-lg';
+      case 5:
+        return 'text-xl';
+      case 6:
+        return 'text-2xl';
+      case 7:
+        return 'text-3xl';
+      case 8:
+        return 'text-4xl';
+      case 9:
+        return 'text-5xl';
+      default:
+        return 'text-base';
+    }
   }
 }
--- client/src/app/views/detail-page/detail-page.ts
+++ client/src/app/views/detail-page/detail-page.ts
@@ -1,6 +1,5 @@
 import { Component, effect, inject, input, OnDestroy } from '@angular/core';
 import { TableModule } from 'primeng/table';
-import { Chip } from 'primeng/chip';
 import { DeleteEntity } from '../../delete-entity/delete-entity';
 import { EditEntity } from '../../edit-entity/edit-entity';
 import { EntityService } from '../../entity.service';
@@ -10,11 +9,12 @@ import { DialogService } from 'primeng/dynamicdialog';
 import { visibleProperties } from '../../utils/utils';
 import { NodeTypes } from '../../statements/node-types/node-types';
 import { Statements } from '../../statements/statements';
+import { PropertyList } from '../../statements/property-list/property-list';
 
 @Component({
   selector: 'app-detail-page',
   providers: [DialogService],
-  imports: [TableModule, Chip, DeleteEntity, EditEntity, ProgressSpinner, CreateAnnotation, NodeTypes, Statements],
+  imports: [TableModule, DeleteEntity, EditEntity, ProgressSpinner, CreateAnnotation, NodeTypes, Statements, PropertyList],
   styleUrl: './detail-page.scss',
   templateUrl: './detail-page.html',
 })
